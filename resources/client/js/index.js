820c31bed30541057cf886235aa9eb134da51c02
Chris <88750@farnborough.ac.uk>
2019-12-06 12:15:26 +0000

    J u s t
    W e r k s

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -1,15 +1,33 @@
 function pageLoad() {
 
-    let now = new Date();
 
+    let now = new Date()
     let myHTML = '<div style="text-align:center;">'
-        + '<h1>Welcome to my API powered website!</h1>'
-        + '<img src="/client/img/logo.png"  alt="Logo"/>'
+        + '<h1>Tracking your progress</h1>'
+        + '<img src="/client/img/papat.gif"  alt="Logo"/>'
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
         + '</div>'
+
         + '</div>';
 
     document.getElementById("testDiv").innerHTML = myHTML;
 
+    fetch('/user/get/4')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            // Work with JSON data here
+
+            document.getElementById("crud").innerHTML = JSON.stringify(data);
+        })
 }
+
+
+
+
+
+
+
+
