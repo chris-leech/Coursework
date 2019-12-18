f8b3b583319fab63572bad2eee1d12f2b3d6582d
Chris <88750@farnborough.ac.uk>
2019-12-18 10:28:37 +0000

    J u s t
    W e r k s

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -5,7 +5,7 @@ function pageLoad() {
     let now = new Date()
     let myHTML = '<div style="text-align:center;">'
         + '<h1>Tracking your progress</h1>'
-        + '<img src="/client/img/patpat.gif"  alt="Logo"/>'
+        + '<img src="/client/img/pa tpat.gif"  alt="Logo"/>'
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
         + '</div>'
@@ -21,9 +21,8 @@ function pageLoad() {
         .then(data => {
             console.log(data);
             let obj = JSON.parse(JSON.stringify(data));
-            console.log(obj);
             // Work with JSON data here
-            console.log(obj.firstname)
+            console.log(obj[0].firstname)
             document.getElementById("crud").innerHTML = JSON.stringify(obj.email);
         })
 }
